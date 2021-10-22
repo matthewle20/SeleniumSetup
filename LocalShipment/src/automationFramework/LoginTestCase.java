db3bfb5241ce781de14201603de182e756083937
Matthew Le <matthewmle20@gmail.com>
2021-10-22 16:24:04 -0500

    Create LoginTestCase.java
    Using Google Chrome WebDriver

---
+++ LocalShipment/src/automationFramework/LoginTestCase.java
@@ -0,0 +1,15 @@
+package automationFramework;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class LoginTestCase {
+
+	public static void main(String[] args) {
+		String path = System.getProperty("user.dir") + "\\drivers\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", path);
+		WebDriver driver = new ChromeDriver(); 
+		driver.get("http://www.google.com/");    
+	}
+
+}
